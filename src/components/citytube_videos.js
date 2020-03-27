576972c46dba931d647f80f0855cd7199ebec9e1
Arnold Csisztai <csisztai.arnold@gmail.com>
2020-03-27 17:35:13 +0100

    Manifest bug fix, unique id for articles

--- src/components/citytube_videos.js
+++ src/components/citytube_videos.js
@@ -4,7 +4,7 @@ const CitytubeVideos = ({citytube_videos}) => {
     return (
         <div className="row">
             {citytube_videos.map((citytube_video) => (
-                <article className="col-lg-4 col-md-6 video-item">
+                <article className="col-lg-4 col-md-6 video-item" key={citytube_video.nid[0].value}>
                     <header style={{ backgroundImage: "url(" + citytube_video.field_thumbnail_url[0].value + ")" }}>
                         <div className="date">
                             {(citytube_video.field_published[0].value).split('T')[0].split('-')[0].slice(-2)}<br />
