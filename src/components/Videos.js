e6bc7a656357acb8bc6e27cc6ff54a6b71a48d34
Arnold Csisztai <csisztai.arnold@gmail.com>
2020-03-31 23:50:42 +0200

    Refactoring

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -2,24 +2,23 @@ import React from "react";
 
 const handleVideoTitleClick = (e) => {
     console.log('Video title clicked.');
-}
+};
 
-const Videos = (props) => {
-    const {videos} = props;
-    const videoList = videos.map(video_item => {
+const Videos = ({videos}) => {
+    const videoList = videos.map(video => {
         return (
-            <article className="col-lg-4 col-md-6 video-item" key={video_item.nid[0].value}
-                     video-title={video_item.title[0].value} channel={video_item.field_channel[0].value}>
-                <header style={{backgroundImage: "url(" + video_item.field_thumbnail_url[0].value + ")"}}>
+            <article className="col-lg-4 col-md-6 video-item" key={video.nid[0].value}
+                     video-title={video.title[0].value} channel={video.field_channel[0].value}>
+                <header style={{backgroundImage: "url(" + video.field_thumbnail_url[0].value + ")"}}>
                     <div className="date">
-                        {(video_item.field_published[0].value).split('T')[0].split('-')[0].slice(-2)}<br/>
-                        {(video_item.field_published[0].value).split('T')[0].split('-')[1]}<br/>
-                        {(video_item.field_published[0].value).split('T')[0].split('-')[2]}
+                        {(video.field_published[0].value).split('T')[0].split('-')[0].slice(-2)}<br/>
+                        {(video.field_published[0].value).split('T')[0].split('-')[1]}<br/>
+                        {(video.field_published[0].value).split('T')[0].split('-')[2]}
                     </div>
                     <div className="header-item-top"/>
                     <div className="header-item">
                         <h1 onClick={handleVideoTitleClick}>
-                            {video_item.title[0].value}
+                            {video.title[0].value}
                         </h1>
                     </div>
                     <div className="header-item-bottom"/>
@@ -27,13 +26,13 @@ const Videos = (props) => {
                 <div className="content">
                     <div className="channel">
                         <a href="#"
-                           title={video_item.field_channel[0].value}>{video_item.field_channel[0].value}</a>
+                           title={video.field_channel[0].value}>{video.field_channel[0].value}</a>
                     </div>
                     <div className="city">
-                        <a href="#">{video_item.field_city[0].target_id}</a>
+                        <a href="#">{video.field_city[0].target_id}</a>
                     </div>
                     <div className="description"
-                         dangerouslySetInnerHTML={{__html: video_item.body[0] && video_item.body[0].value}}/>
+                         dangerouslySetInnerHTML={{__html: video.body[0] && video.body[0].value}}/>
                 </div>
             </article>
         )
