d7c157a859c61a822bc784327a2f245fd53c7cb3
Arnold Csisztai <csisztai.arnold@gmail.com>
2020-04-03 22:59:25 +0200

    Set state from form data

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,32 @@ import React, {Component} from 'react'
 class Contact extends Component {
 
     state = {
-    }
+        name: null,
+        email: null,
+        message: null,
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    };
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Contact form submitted');
-    }
+        console.log(this.state);
+    };
 
     render() {
         return (
             <div className="contact">
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="name">Name:</label>
-                    <input id="name" type="text" placeholder="Your name" />
+                    <input id="name" type="text" required="required" placeholder="Your name" onChange={this.handleChange} />
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" type="email" required="required" placeholder="Your email" onChange={this.handleChange} />
                     <label htmlFor="message">Message:</label>
-                    <textarea id="message"></textarea>
+                    <textarea id="message" required="required" onChange={this.handleChange}></textarea>
                     <button>Submit</button>
                 </form>
             </div>
